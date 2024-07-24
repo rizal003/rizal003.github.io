54e7bb132bcb2d57c5af4fc3fc0a82b42117073e
KhRizal <107873449+rizal003@users.noreply.github.com>
2024-07-24 10:14:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,40 @@
-// Smooth Scrolling - modified to account for the video section
-document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        var targetElement = document.querySelector(this.getAttribute('href'));
-        window.scrollTo({
-            top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
-            behavior: 'smooth'
-        });
-    });
-});
 
-// Enhance interactivity on mouse enter
-document.querySelectorAll('section').forEach(function(section) {
-    section.addEventListener('mouseenter', function() {
-        console.log('You are viewing: ' + section.querySelector('h2').textContent);
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyVideos = document.querySelectorAll('video[preload="none"]');
-    var observer = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                var video = entry.target;
-                video.setAttribute('preload', 'auto');
-                observer.unobserve(video);
-            }
-        });
-    });
-
-    lazyVideos.forEach(function(video) {
-        observer.observe(video);
-    });
-});
-
-// Youtube video interactions
+// Hamburger Menu
 document.addEventListener('DOMContentLoaded', function() {
-    var imageContainer = document.getElementById('imageToVideo');
-    var imageContainer2 = document.getElementById('imageToVideo2');
-    var isVideoDisplayed = false;
-    var isVideoDisplayed2 = false;
+    // Select the hamburger menu icon
+    var hamburger = document.querySelector('.hamburger-menu');
+    // Select the navigation list
+    var navUL = document.querySelector('nav ul');
+    // Select all links within the navigation list
+    var navLinks = document.querySelectorAll('nav ul li a');
 
-    imageContainer.addEventListener('click', function() {
-        if (!isVideoDisplayed) {
-            imageContainer.innerHTML = '<iframe src="https://www.youtube.com/embed/6K-ATmU-NbU?si=IVihq-NY68Zz7J_u" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>';
-            isVideoDisplayed = true;
-        } else {
-            imageContainer.innerHTML = '<img src="images/mouse_history1.png" alt="History of the Computer Mouse" id="historyImage" style="width: 100%; height: auto;">';
-            isVideoDisplayed = false;
-        }
-    });
+    // Function to open or close the navigation menu
+    function toggleMenu() {
+        // Toggle the left position to show or hide the menu
+        navUL.style.left = navUL.style.left === '0px' ? '-100%' : '0px';
+    }
 
-    imageContainer2.addEventListener('click', function() {
-        if (!isVideoDisplayed2) {
-            imageContainer2.innerHTML = '<iframe src="https://www.youtube.com/embed/Kc3HXYkDzBg?si=iqDjDFNa9bmIe1Ey" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>';
-            isVideoDisplayed2 = true;
-        } else {
-            imageContainer2.innerHTML = '<img src="images/mouse_history2.png" alt="Continued History of the Computer Mouse" id="historyImage" style="width: 100%; height: auto;">';
-            isVideoDisplayed2 = false;
-        }
-    });
-});
+    // Add click event listener to hamburger icon to trigger menu toggle
+    hamburger.addEventListener('click', toggleMenu);
 
-// Modal interactions
-document.querySelectorAll('.overlay').forEach(function(overlay) {
-    overlay.addEventListener('click', function() {
-        location.href = '#historySection';
-        location.href = '#buildSection';
-        location.href = '#typesOfMouse';
-    });
-    overlay.addEventListener('keypress', function(e) {
-        if (e.key === 'Enter') {
-            location.href = '#historySection';
-            location.href = '#buildSection';
-            location.href = '#typesOfMouse';
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var sliders = document.querySelectorAll('.type-image');
-
-    sliders.forEach(function(slider) {
-        var images = slider.querySelectorAll('.slider-image');
-        var prevButton = slider.querySelector('.prev-btn');
-        var nextButton = slider.querySelector('.next-btn');
-        var currentImage = 0;
-
-        function updateImageDisplay() {
-            images.forEach(function(img, index) {
-                img.style.display = index === currentImage ? 'block' : 'none';
+    // Add click event listeners to each navigation link
+    Array.prototype.forEach.call(navLinks, function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            // Navigate to the section specified in href attribute of the link
+            var targetSection = document.querySelector(this.getAttribute('href'));
+            // Smooth scroll to the target section, offset by header height
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
             });
-        }
-
-        prevButton.addEventListener('click', function() {
-            currentImage = (currentImage - 1 + images.length) % images.length;
-            updateImageDisplay();
+            // Close the navigation menu after selecting a section
+            toggleMenu();
         });
-
-        nextButton.addEventListener('click', function() {
-            currentImage = (currentImage + 1) % images.length;
-            updateImageDisplay();
-        });
-
-        updateImageDisplay();
     });
 });
 
+// HTML texts hidden
 document.addEventListener('DOMContentLoaded', function() {
     var overlays = document.querySelectorAll('.overlay');
 
@@ -132,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// Header visibility on scroll
+// Headers gone when user scroll 
 var lastScrollTop = 0;
 var header = document.querySelector('header');
 
@@ -147,7 +75,7 @@ window.addEventListener('scroll', function() {
     lastScrollTop = scrollTop;
 });
 
-// Text animation on visibility
+// Text animation (fade-slide)
 document.addEventListener('DOMContentLoaded', function() {
     var elements = document.querySelectorAll('.fade-in-slide-up');
 
@@ -167,11 +95,149 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+//3 videos Types, History, Build
 
-// CPS GAME
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Select all video elements with a specific attribute
+    var lazyVideos = document.querySelectorAll('video[preload="none"]');
+    // Create an observer to check visibility of these videos
+    var observer = new IntersectionObserver(function(entries, observer) {
+        // Iterate through each video that becomes visible
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                // Change the preload attribute to start loading the video
+                var video = entry.target;
+                video.setAttribute('preload', 'auto');
+                // Stop observing the video once it starts loading
+                observer.unobserve(video);
+            }
+        });
+    });
+        // Start observing each lazy-load video
+        lazyVideos.forEach(function(video) {
+        observer.observe(video);
+    });
+});
+
+
+// Youtube video interactions
+document.addEventListener('DOMContentLoaded', function() {
+    var imageContainer = document.getElementById('imageToVideo');
+    var imageContainer2 = document.getElementById('imageToVideo2');
+    var isVideoDisplayed = false;
+    var isVideoDisplayed2 = false;
+
+    // Toggle display between image and video for the first container
+    imageContainer.addEventListener('click', function() {
+        if (!isVideoDisplayed) {
+            // Replace image with a video
+            imageContainer.innerHTML = '<iframe src="https://www.youtube.com/embed/6K-ATmU-NbU?si=IVihq-NY68Zz7J_u" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>';
+            isVideoDisplayed = true;
+        } else {
+            // Replace video with an image
+            imageContainer.innerHTML = '<img src="images/mouse_history1.png" alt="History of the Computer Mouse" id="historyImage" style="width: 100%; height: auto;">';
+            isVideoDisplayed = false;
+        }
+    });
+
+    // Toggle display between image and video for the second container
+    imageContainer2.addEventListener('click', function() {
+        if (!isVideoDisplayed2) {
+            // Replace image with a video
+            imageContainer2.innerHTML = '<iframe src="https://www.youtube.com/embed/Kc3HXYkDzBg?si=iqDjDFNa9bmIe1Ey" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>';
+            isVideoDisplayed2 = true;
+        } else {
+            // Replace video with an image
+            imageContainer2.innerHTML = '<img src="images/mouse_history2.png" alt="Continued History of the Computer Mouse" id="historyImage" style="width: 100%; height: auto;">';
+            isVideoDisplayed2 = false;
+        }
+    });
+});
+
+// Modal interactions 
+document.querySelectorAll('.overlay').forEach(function(overlay) {
+    overlay.addEventListener('click', function() {
+        location.href = '#historySection';
+        location.href = '#buildSection';
+        location.href = '#typesOfMouse';
+    });
+    overlay.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            location.href = '#historySection';
+            location.href = '#buildSection';
+            location.href = '#typesOfMouse';
+        }
+    });
+});
+
+
+
+// Build of mouse part, switching of images when clicked
+document.addEventListener('DOMContentLoaded', function() {
+    var firstBuildImage = document.getElementById('firstBuildImage');
+    var secondBuildImage = document.getElementById('secondBuildImage');
+
+    // Set event listener for the first build image to toggle on click
+    firstBuildImage.addEventListener('click', function() {
+        toggleImage(this);
+    });
+
+    // Set event listener for the second build image to toggle on click
+    secondBuildImage.addEventListener('click', function() {
+        toggleImage(this);
+    });
+
+    // Function to swap the displayed image and its alternate source
+    function toggleImage(imageContainer) {
+        var imgElement = imageContainer.getElementsByTagName('img')[0];
+        var currentSrc = imgElement.src;
+        var altSrc = imageContainer.getAttribute('data-alt-src');
+        // Swap the source of the image
+        imgElement.src = altSrc;
+        imageContainer.setAttribute('data-alt-src', currentSrc);
+    }
+});
+
+// Functionality for the slider in the type of mouse section
+document.addEventListener('DOMContentLoaded', function() {
+    var sliders = document.querySelectorAll('.type-image');
+
+    // Set up the slider functionality for each type of mouse image container
+    sliders.forEach(function(slider) {
+        var images = slider.querySelectorAll('.slider-image');
+        var prevButton = slider.querySelector('.prev-btn');
+        var nextButton = slider.querySelector('.next-btn');
+        var currentImage = 0;
+
+        // Function to update the display of images within the slider
+        function updateImageDisplay() {
+            images.forEach(function(img, index) {
+                img.style.display = index === currentImage ? 'block' : 'none';
+            });
+        }
+
+        // Set event listener for the previous button to move to the previous image
+        prevButton.addEventListener('click', function() {
+            currentImage = (currentImage - 1 + images.length) % images.length;
+            updateImageDisplay();
+        });
+
+        // Set event listener for the next button to move to the next image
+        nextButton.addEventListener('click', function() {
+            currentImage = (currentImage + 1) % images.length;
+            updateImageDisplay();
+        });
+
+        // Initialize the display of the first image
+        updateImageDisplay();
+    });
+});
+
 // CPS GAME
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Getting required elements from the DOM by their IDs
     var clickArea = document.getElementById('clickArea');
     var durationSelect = document.getElementById('durationSelect');
     var restartButton = document.getElementById('restartButton');
@@ -180,56 +246,62 @@ document.addEventListener('DOMContentLoaded', function() {
     var clicksPerSecond = document.getElementById('clicksPerSecond');
     var modal = document.getElementById('resultModal');
     var closeButton = document.querySelector('.close-button');
+
+    // Initializing game variables
     var timeLeft = 0;
     var clicks = 0;
     var gameActive = false;
     var startTime;
     var timer;
 
+    // Close modal when close button is clicked
     closeButton.addEventListener('click', function() {
         modal.style.display = "none";
     });
 
+    // Reset the game when restart button is clicked
     restartButton.addEventListener('click', function() {
-        if (timer) clearInterval(timer);
-        resetGame();
+        if (timer) clearInterval(timer);  // Clear any running timer
+        resetGame();  // Call resetGame to reset game settings
     });
 
+    // Handle click events on the click area
     clickArea.addEventListener('click', function(event) {
-        if (!gameActive) {
+        if (!gameActive) {  // Start the game if not already active
             startGame();
-        } else {
+        } else {  // If game is active, register clicks and update display
             clicks++;
             numberOfClicks.textContent = clicks;
             updateCPS();
-            createVisualFeedback(event);
+            createVisualFeedback(event);  // Visual feedback for the click
         }
     });
 
+    // Function to create a visual feedback on click
     function createVisualFeedback(event) {
-        var dot = document.createElement('div');
+        var dot = document.createElement('div');  // Create a dot element
         dot.className = 'dot';
         var rect = clickArea.getBoundingClientRect();
-        var x = event.clientX - rect.left;
-        var y = event.clientY - rect.top;
+        var x = event.clientX - rect.left;  // X position of the click
+        var y = event.clientY - rect.top;  // Y position of the click
 
+        // Style and position the dot
         dot.style.left = (x - 5) + 'px';
         dot.style.top = (y - 5) + 'px';
+        dot.style.backgroundColor = getRandomColor();  // Random color for the dot
 
-        dot.style.backgroundColor = getRandomColor();
-
+        // Add dot to the click area and animate its disappearance
         clickArea.appendChild(dot);
-
         setTimeout(function() {
             dot.style.opacity = '0';
             dot.style.transform = 'scale(10)';
         }, 100);
-
         setTimeout(function() {
             clickArea.removeChild(dot);
         }, 600);
     }
 
+    // Generate a random color for the dot
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -239,6 +311,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return color;
     }
 
+    // Start the game setup
     function startGame() {
         gameActive = true;
         clicks = 0;
@@ -249,6 +322,7 @@ document.addEventListener('DOMContentLoaded', function() {
         clickArea.textContent = 'Click as fast as you can!';
         startTime = Date.now();
 
+        // Setup and start the timer
         timer = setInterval(function() {
             timeLeft--;
             timeRemaining.textContent = timeLeft;
@@ -258,6 +332,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 1000);
     }
 
+    // Reset the game to initial state
     function resetGame() {
         clicks = 0;
         timeLeft = parseInt(durationSelect.value);
@@ -268,12 +343,14 @@ document.addEventListener('DOMContentLoaded', function() {
         clickArea.textContent = 'Click here to start the game!';
     }
 
+    // Update clicks per second display
     function updateCPS() {
         var elapsed = (Date.now() - startTime) / 1000;
         var cps = (clicks / elapsed).toFixed(2);
         clicksPerSecond.textContent = cps;
     }
 
+    // End the game and display results
     function endGame() {
         clearInterval(timer);
         gameActive = false;
@@ -284,6 +361,7 @@ document.addEventListener('DOMContentLoaded', function() {
         modal.style.display = "block";
     }
 
+    // Determine the image based on CPS
     function getImageForCPS(cps) {
         cps = parseFloat(cps);
         if (cps < 3) {
@@ -297,28 +375,6 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             return 'images/falcon.png';
         }
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var firstBuildImage = document.getElementById('firstBuildImage');
-    var secondBuildImage = document.getElementById('secondBuildImage');
-
-    firstBuildImage.addEventListener('click', function() {
-        toggleImage(this);
-    });
-
-    secondBuildImage.addEventListener('click', function() {
-        toggleImage(this);
-    });
-
-    function toggleImage(imageContainer) {
-        var imgElement = imageContainer.getElementsByTagName('img')[0];
-        var currentSrc = imgElement.src;
-        var altSrc = imageContainer.getAttribute('data-alt-src');
-
-        imgElement.src = altSrc;
-        imageContainer.setAttribute('data-alt-src', currentSrc);
     }
 });
 
